64f07731031f4024cce349e76ec03ccf479464a3
“invinciblebeing0” <shaykhunkabir@gmail.com>
2022-12-09 15:07:39 -0800

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: 0
+ */
+int main(void)
+{
+	char c, d;
+
+	c = 'a';
+	d = 'A';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
