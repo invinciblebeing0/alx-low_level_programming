00249fd881565917f43451f398122112c4c7c0bd
“invinciblebeing0” <shaykhunkabir@gmail.com>
2022-12-13 22:04:45 -0800

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - Entry
+ *@c: char to be checked
+ * Return: 1 if c is a char, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	char c;
+
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
