e792fe4bea498f7ec5b11352f3fde7a9ad344016
“invinciblebeing0” <shaykhunkabir@gmail.com>
2022-12-13 23:23:28 -0800

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Entry
+ * @int: the value to check
+ * Return: 0
+ */
+int _abs(int)
+{
+	int c;
+
+	if (n < 0)
+		_putchar(n * -1);
+	else
+		_putchar(n);
+	return (0);
+}
