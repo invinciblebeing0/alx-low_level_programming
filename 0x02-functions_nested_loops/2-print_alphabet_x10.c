37afb50aa374efa94432d7341b5e547ff5fc56aa
“invinciblebeing0” <shaykhunkabir@gmail.com>
2022-12-13 20:29:13 -0800

    10 alpha

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	char c = 'a';
+
+	while (i <= 9)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		i++;
+	}
+}
