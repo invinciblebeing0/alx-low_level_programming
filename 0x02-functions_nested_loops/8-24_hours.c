7c82c2529ad152db595410ef680925348566051d
“invinciblebeing0” <shaykhunkabir@gmail.com>
2022-12-14 19:42:22 -0800

    time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry
+ * Return: Always 0
+ */
+void jack_bauer(void)
+{
+	int hrs, min;
+
+	for (hrs = 0; hrs <= 23; hrs++)
+	{
+		for (min = 0; min <= 59; min++)
+		{
+			_putchar((hrs / 10) + '0');
+			_putchar((hrs % 10) + '0');
+			_putchar(' : ');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
