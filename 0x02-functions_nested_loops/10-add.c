95ae8fdc970469019b23eb0f79431d983fd9f479
“invinciblebeing0” <shaykhunkabir@gmail.com>
2022-12-14 20:52:51 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Entry
+ * Return: results of adddition
+ * @a: number to compute
+ * @b: number to compute
+ */
+int add(int a, int b)
+{
+	int r = a + b;
+
+	return (r);
+}
